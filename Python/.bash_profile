c04e4861da2f1d1eff7ce01701fc30d45bbe2fca
Renger Bierenga <bierengarenger@gmail.com>
2024-09-04 13:53:26 +0200

    Made a .bash_profile file with some aliases for co mmands to use the Figlets.py file.

---
+++ Python/.bash_profile
@@ -0,0 +1,3 @@
+alias fig="python Figlets.py"
+alias setstandard="sed -i \"s/slant/standard/\" Figlets.py"
+alias setslant="sed -i \"s/standard/slant/\" Figlets.py"
---
+++ Python/Figlets.py
@@ -0,0 +1,21 @@
+from pyfiglet import Figlet
+import sys
+
+# Get the variable value from the command-line arguments
+if len(sys.argv) == 1 or len(sys.argv) >= 4:
+    print("Usage: fig <text> <font>")
+    sys.exit(1)
+
+string = sys.argv[1]
+
+if len(sys.argv) == 3:
+    setFont = sys.argv[2]
+else:
+    setFont = 'slant'
+
+# Create a Figlet instance with a chosen font
+figlet = Figlet(font=setFont)  # Replace 'script' with a font you like
+
+# Generate text in the chosen font
+text = figlet.renderText(string)
+print(text)
