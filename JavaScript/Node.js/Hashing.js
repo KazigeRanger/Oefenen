416f8d90177c84a074ae582cecb04e49321bec5e
Renger Bierenga <bierengarenger@gmail.com>
2024-09-06 08:44:35 +0200

    Added code in Hashing.js that allows the user to input their password and then their input will be verified by checking if their input matches with the stored hash.

--- JavaScript/Node.js/Hashing.js
+++ JavaScript/Node.js/Hashing.js
@@ -1,18 +1,26 @@
 const {
     createHash,
   } = require('node:crypto');
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
 const storedHash = "a570819e95aa4c43ad54fbb7ea3578726ee0eb1185348564908e5159822d653b";
 
-const passwordInput = "Broodje ham";
+rl.question('What is your password? ', (passwordInput) => {
+    const passwordInputHash = createHash('sha256').update(passwordInput).digest('hex');
 
-const passwordInputHash = createHash('sha256').update(passwordInput).digest('hex');
+    console.log("The stored hash is: " + storedHash);
+    console.log("The input hash is: " + passwordInputHash);
 
-console.log("The stored hash is: " + storedHash);
-console.log("The input hash is: " + passwordInputHash);
-
-if (storedHash === passwordInputHash) {
-    return console.log("The password is correct.");
-} else if (storedHash !== passwordInputHash) {
-    return console.log("The password is incorrect.");
-}
+    if (storedHash === passwordInputHash) {
+        console.log("The password is correct.");
+        return rl.close();
+    } else if (storedHash !== passwordInputHash) {
+        console.log("The password is incorrect.");
+        return rl.close();
+    }
+});
