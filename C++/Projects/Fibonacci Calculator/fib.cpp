5d3bf27b39e5d7100e6f943b6edc0052a9a7cfc1
Renger Bierenga <bierengarenger@gmail.com>
2024-11-05 19:49:55 +0100

    Tried to get the largeIntAdd() function within fib.cpp working, but I ended up realising that shit was messed up and started all over in largeArithmetic.cpp. Beforehand I fixed the problems with the method I was using in fib.cpp to do large integer addition and implemented the improved method in largeArithmetic.cpp. Now the large integer addition function is fully functional.

--- C++/Projects/Fibonacci Calculator/fib.cpp
+++ C++/Projects/Fibonacci Calculator/fib.cpp
@@ -11,27 +11,58 @@ using namespace std;
 auto start = chrono::high_resolution_clock::now();
 typedef struct largeInt {
     string value;
-    vector<string> chops;
 } largeInt;
 
-vector<string> chop(string str) {
-    int strLength = str.length();
+// string largeIntAdd(largeInt int1, largeInt int2) {
+//     stack<int> int1Stack;
+//     stack<int> int2Stack;
+//     for (char charDigit : int1.value) {
+//         int intDigit = charDigit - '0';
+//         int1Stack.push(intDigit);
+//     }
+//     for (char charDigit : int2.value) {
+//         int intDigit = charDigit - '0';
+//         int2Stack.push(intDigit);
+//     }
 
-    int chopSize = 16;
-    int amountOfChopsNeeded = strLength/chopSize+1;
-    cout << "amount of chops needed: " << amountOfChopsNeeded << endl;
+//     int carriedTerm = 0;
+//     int iterationCount = min(int1Stack.size(), int2Stack.size());
+//     stack<int> resultStack;
+//     for (int i = 0; i < iterationCount; i++) {
+//         // if (i == iterationCount-1) {
+//         //     resultStack.push(((max(int1Stack.size(), int2Stack.size()) == int1Stack.size() ? int1 : int2).value[max(int1Stack.size(), int2Stack.size())-min(int1Stack.size(), int2Stack.size())]-'0')+carriedTerm);
+//         // } else {
+//             int term = int1Stack.top() + int2Stack.top();
+//             int totalTerm = term + carriedTerm;
+//             if (totalTerm >= 10) {
+//                 totalTerm = totalTerm - 10;
+//                 carriedTerm = 1;
+//                 resultStack.push(totalTerm);
+//                 int1Stack.pop();
+//                 int2Stack.pop();
+//             } else {
+//                 carriedTerm = 0;
+//                 resultStack.push(totalTerm);
+//                 int1Stack.pop();
+//                 int2Stack.pop();
+//             }
+//         // }
+//     }
+//     largeInt result;
 
-    vector<string> chops;
-    for (int i = 0; i < amountOfChopsNeeded; i++) {
-        if (i < amountOfChopsNeeded-1) {
-            chops.push_back(str.substr(chopSize*i, chopSize));
-        } else if (i == amountOfChopsNeeded-1) {
-            chops.push_back(str.substr(chopSize*i, strLength-(amountOfChopsNeeded-1)*chopSize));
-        }
-    }
+//     // result.value += (max(int1Stack.size(), int2Stack.size())==int1Stack.size()) ? int1.value.substr(0, int1Stack.size()-int2Stack.size()) : int2.value.substr(0, int2Stack.size()-int1Stack.size());
 
-    return chops;
-}
+//     // if ((int1.value[0]-'0')+(int2.value[0]-'0') >= 10) {
+//     //     result.value += 1+'0';
+//     // }
+//     int resultIterationCount = resultStack.size();
+//     for (int i = 0; i < resultIterationCount; i++) {
+//         result.value += resultStack.top()+'0';
+//         resultStack.pop();
+//     }
+
+//     return result.value;
+// }
 
 string largeIntAdd(largeInt int1, largeInt int2) {
     stack<int> int1Stack;
@@ -44,10 +75,8 @@ string largeIntAdd(largeInt int1, largeInt int2) {
         int intDigit = charDigit - '0';
         int2Stack.push(intDigit);
     }
-
     cout << "int1Stack.size(): " << int1Stack.size() << endl;
     cout << "int2Stack.size(): " << int2Stack.size() << endl;
-
     int carriedTerm = 0;
     int iterationCount = max(int1Stack.size(), int2Stack.size());
     stack<int> resultStack;
@@ -89,8 +118,25 @@ string largeIntAdd(largeInt int1, largeInt int2) {
         // cout << "resultStack.top(): " << resultStack.top() << "\t" << i << endl;
         resultStack.pop();
     }
-
     return result.value;
+}
+
+string calculateLargeNthFib(int n) {
+    largeInt firstFib;
+    largeInt secondFib;
+    firstFib.value = "0";
+    secondFib.value = "1";
+    vector<largeInt> fibSequence;
+    fibSequence.push_back(firstFib);
+    fibSequence.push_back(secondFib);
+    largeInt currentFib;
+    for (int i = 2; i < n; i++) {
+        currentFib.value = largeIntAdd(fibSequence[i-2], fibSequence[i-1]);
+        fibSequence.push_back(currentFib);
+        cout << currentFib.value << endl;
+    }
+
+    return fibSequence[n-1].value;
 }
 
 int main(int argc, char* argv[]) {
@@ -101,24 +147,30 @@ int main(int argc, char* argv[]) {
 
     largeInt large1;
     largeInt large2;
-    large1.value = "65927679461576810473862977549509173951200069903945853";
-    large2.value = "20983467809234789608927109872468097204689723984670937";
+    large1.value = "659276794615768104738620023097697737986690963290734028799098669829349870476879236977549509173951200069903945853";
+    large2.value = "409834678092347896089271098722364798087329683749823890743498502594807628097408920789746468096548739746389476937";
 
     largeInt addedResult;
     addedResult.value = largeIntAdd(large1, large2);
     cout << "Result: " << addedResult.value << endl;
-    // cout << "number of digits: " << large1.value.length() << endl;
+    cout << "number of digits: " << large1.value.length() << endl;
 
-    // large1.chops = chop(large1.value);
+    string test = "1069111472708116000827891121820062536074020647040557919542597172424157498574288157767295977270499939816293422790";
+    if (test == addedResult.value) {
+        cout << "CORRECT" << endl;
+    } else {
+        cout << "INCORRECT" << endl;
+    }
 
-    // for (string chop : large1.chops) {
-    //     cout << "chop: " << chop << endl;
-    // }
+    // largeInt nthFib;
+    // int n = 10;
+    // nthFib.value = calculateLargeNthFib(n);
+    // cout << n << "th fib: " << nthFib.value << endl;
 
-    auto end = chrono::high_resolution_clock::now();
+    // auto end = chrono::high_resolution_clock::now();
 
-    chrono::duration<double> duration = end - start;
-    cout << "Runtime: " << duration.count() << " seconds" << endl;
+    // chrono::duration<double> duration = end - start;
+    // cout << "Runtime: " << duration.count() << " seconds" << endl;
 
     return 0;
 }
---
+++ C++/Projects/Fibonacci Calculator/largeArithmetic.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+
+// auto start = chrono::high_resolution_clock::now();
+
+typedef struct largeInt {
+    string value;
+} largeInt;
+
+largeInt addLarge(largeInt firstLarge, largeInt secondLarge) {
+    // Store the lengths of the values of the integers to add
+    int firstLength = firstLarge.value.length();
+    int secondLength = secondLarge.value.length();
+
+    cout << "1st Length: " << firstLength << endl;
+    cout << "2nd Length: " << secondLength << endl;
+
+    // Store the reversed int values to loop over
+    // string firstReversed = firstLarge.value;
+    // string secondReversed = secondLarge.value;
+
+    // reverse(firstReversed.begin(), firstReversed.end());
+    // reverse(secondReversed.begin(), secondReversed.end());
+
+    // Add the necessary amount of zeroes to the .value strings of the 
+    // largeInts to add.
+    largeInt& biggestInt = (max(firstLength, secondLength) == firstLength) ? firstLarge : secondLarge;
+    largeInt& smallestInt = (min(firstLength, secondLength) == secondLength) ? secondLarge : firstLarge;
+    biggestInt.value.insert(0, 1, '0');
+    smallestInt.value.insert(0, biggestInt.value.length()-smallestInt.value.length(), '0');
+    reverse(biggestInt.value.begin(), biggestInt.value.end());
+    reverse(smallestInt.value.begin(), smallestInt.value.end());
+
+    cout << "First int: " << firstLarge.value << endl;
+    cout << "Second int: " << secondLarge.value << endl;
+
+    // Define a stack to store the result of each individual addition
+    stack<char> resultStack;
+
+    // Define the amount of iterations needed to do the addition
+    int iterationCount = firstLarge.value.length();
+
+    // Loop over the integer values to do the addition
+    int carriedTerm = 0;
+    for (int i = 0; i < iterationCount; i++) {
+        int term = (firstLarge.value[i]-'0') + (secondLarge.value[i]-'0') + carriedTerm;
+        if (term >= 10) {
+            term -= 10;
+            carriedTerm = 1;
+            resultStack.push(term+'0');
+        } else if (term < 10) {
+            resultStack.push(term+'0');
+            carriedTerm = 0;
+        }
+    }
+
+    cout << "Result Stack Size: " << resultStack.size() << endl;
+
+    // Define a largeInt to store the result value of the addition
+    largeInt result;
+
+    // Loop over the resultStack to construct the value of the result
+    int resultIterationCount = resultStack.size();
+    for (int i = 0; i < resultIterationCount; i++) {
+        result.value += resultStack.top();
+        resultStack.pop();
+    }
+
+    if (result.value[0] == '0') {
+        result.value = result.value.substr(1, result.value.length()-1);
+        return result;
+    } else {
+        return result;
+    }
+}
+
+int main() {
+    // Keep track of the runtime of the program
+    auto start1 = chrono::high_resolution_clock::now();
+
+    // Code to run
+    largeInt firstInteger;
+    largeInt secondInteger;
+
+    firstInteger.value = "92386720938476";
+    secondInteger.value = "345734584345";
+
+    largeInt additionResult = addLarge(firstInteger, secondInteger);
+    cout << "Addition result: " << additionResult.value << endl;
+    cout << "Result Length: " << additionResult.value.length() << endl;
+
+    // Keep track of the runtime of the program
+    auto end1 = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration1 = end1 - start1;
+    cout << "Runtime: " << duration1.count() << " seconds" << endl;
+
+    return 0;
+}
